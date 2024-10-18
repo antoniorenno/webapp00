2a433be697cb0b39563939d1b759a60f4b7f026b
antoniorenno <165731183+antoniorenno@users.noreply.github.com>
2024-10-18 20:41:08 -0300

    Update webapp00.py
    correção

--- webapp00.py
+++ webapp00.py
@@ -14,10 +14,10 @@ st.subheader("Aula para subir site simples")
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("realizando texto para testar as alterações do site")
 
-values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
-st.write("Values:", values)
-
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFwxxM13bxUC0dpyd0w0PxfZIrJ-hp4Px-R6rsTiG3c3n-89JApzA0jYJpU9vNfxeNCvtJ0Cg35KtO/pub?gid=556192647&single=true&output=csv"
 db = Ler_GooglePlanilha(url)
 db.fillna('', inplace=True)
 Escrever(db)
+
+values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
+st.write("Values:", values)
